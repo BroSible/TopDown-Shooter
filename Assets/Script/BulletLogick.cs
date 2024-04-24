db79d3b43c86d0f1cf663714e2ce5e62b2086581
BroSible <mikhail.petr2005@gmail.com>
2024-04-25 00:58:42 +0300

    Enemy take damage
    Enemy can take damage and bullet has been destroyed, when their collider compare with beetle.

---
+++ Assets/Script/BulletLogick.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogick : MonoBehaviour
+{
+    public BaseWeapon baseWeapon;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            BaseEnemy enemy = other.GetComponent<BaseEnemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(baseWeapon.weaponDamage);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
